9f9143c07c424e0298cc84feaff7bb5a8ec2038d
gdent <gavin.dent@albionsc.co.uk>
2017-06-21 14:00:58 +0100

    Fixed linting

--- app/src/app/App.js
+++ app/src/app/App.js
@@ -4,7 +4,6 @@ import React from 'react'
 class App extends React.Component {
     
     render() {
-        var thingShouldFail;
         return (
             <div>
                 <p>Congratulations, you now have a useless React application</p>
