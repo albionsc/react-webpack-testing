c09b0d5c450ab565c6f2c670c5ff5b0c557a73d6
gdent <gavin.dent@albionsc.co.uk>
2017-06-25 19:12:39 +0100

    Added basic test for react form

--- app/src/app/App.js
+++ app/src/app/App.js
@@ -1,13 +1,45 @@
 import React from 'react'
-
+import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator'
+import RaisedButton from 'material-ui/RaisedButton'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin  from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 class App extends React.Component {
     
+    constructor(props){
+        super(props);
+        this.state = {
+            textualValue: ''
+        }
+    }
+
+    handleSubmit() {
+        this.props.onSubmit();
+    }
+
+    handleError() {
+        this.props.onError();
+    }
+
+
     render() {
         return (
-            <div>
-                <p>Congratulations, you now have a useless React application</p>
-            </div>
+            <MuiThemeProvider>
+                <ValidatorForm ref="form"
+                    onSubmit={this.handleSubmit.bind(this)}
+                    onError={this.handleError.bind(this)}>
+                    <p>Congratulations, you now have a useless React application</p>
+                    <TextValidator
+                        floatingLabelText="Textual"
+                        onChange={this.handleChange}
+                        name="textual"
+                        value={this.state.textualValue}
+                        validators={['required']}
+                        errorMessages={['this field is required']} />
+                    <RaisedButton className="app__button__submit" type="submit" value="submit" label="submit" />
+                </ValidatorForm>
+            </MuiThemeProvider>
         )
     }
 }
--- app/src/app/App.test.js
+++ app/src/app/App.test.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { expect, assert } from 'chai';
+import { mount } from 'enzyme';
+import Sinon from 'sinon';
 
 import App from './App';
 
 describe('<App />', () => {
+
+
+  var mockSubmit = Sinon.spy();
+  var mockError = Sinon.spy();
+
   it('renders a single <p>', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('p')).to.have.length(1);
+    const wrapper = mount(<App onSubmit={mockSubmit} onError={mockError}/>);
+    wrapper.find('button').simulate('submit');
+    expect(mockError.calledOnce).to.equal(true);
+
+    wrapper.setState({
+      textualValue: "Im a real value"
+    });
+    //click doesnt work in this context have to use the submit event
+    wrapper.find('button').simulate('submit');
+    expect(mockSubmit.calledOnce).to.equal(true);
+
   });
 });
